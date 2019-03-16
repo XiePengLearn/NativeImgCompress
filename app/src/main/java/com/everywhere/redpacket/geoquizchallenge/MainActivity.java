6dff7b2d8b659fe7341a4645170eb86c99027252
Xie Peng <769783182@qq.com>
2019-03-16 15:56:14 +0800

    Initial commit

---
+++ app/src/main/java/com/everywhere/redpacket/geoquizchallenge/MainActivity.java
@@ -0,0 +1,16 @@
+package com.everywhere.redpacket.geoquizchallenge;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView button = (TextView) findViewById(R.id.hello_world);
+
+    }
+}
